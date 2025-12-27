af361c485154ff90b2cbd2368e672c1827609a2d
Sedef Bozkurt <sedefbozkurt.4379@gmail.com>
2025-12-27 20:15:23 +0300

    Initial SwiftUI project setup

---
+++ SimpleInfoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleInfoApp
+//
+//  Created by Sedef Bozkurt on 27.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleInfoApp/SimpleInfoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleInfoAppApp.swift
+//  SimpleInfoApp
+//
+//  Created by Sedef Bozkurt on 27.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct SimpleInfoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
